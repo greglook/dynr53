b9d43cd358361cce4463f849c836fd4cb2617f7d
Greg Look <greg@greg-look.net>
2014-03-02 19:35:45 +0000

    Add more options to update script.

--- update-routes.rb
+++ update-routes.rb
@@ -20,21 +20,33 @@ require 'system/getifaddrs'
 ##### CONFIGURATION #####
 
 $hosted_zone = nil
+$domain_name = %x{hostname --fqdn}.strip
 $ipv4_interfaces = []
 $ipv6_interfaces = []
-$domain_name = %x{hostname --fqdn}.strip
-$create_records = true
+
 $ttl = 300
+$create_records = true
+$await_propagation = true
+
+$verbose = false
 
 # Parse command-line options.
 options = OptionParser.new do |opts|
-  opts.banner = "Usage: #{File.basename($0)} [options] <interfaces> <hosted zone id>"
-  opts.on('-4', '--ipv4 IFACE',    "Updates A records for the IPv4 address on an interface") {|iface| $ipv4_interfaces << iface }
-  opts.on('-6', '--ipv6 IFACE',    "Updates AAAA records for global IPv6 addresses on an interface") {|iface| $ipv6_interfaces << iface }
-  opts.on('-f', '--fqdn HOSTNAME', "Sets the fully-qualified domain name to update (default: #{$domain_name})") {|n| $domain_name = n }
-  opts.on('-t', '--ttl SECONDS',   "Sets the time-to-live of the updated records (default: #{$ttl})") {|t| $ttl = t.to_i }
-  opts.on('-c', '--[no-]create',   "Create DNS records if they do not exist (default: #{$create_records})") {|v| $create_records = v }
-  opts.on('-h', '--help',          "Displays usage information") { print opts; exit }
+  opts.banner = "Usage: #{File.basename($0)} [options] <hosted zone id>"
+  opts.separator ""
+  opts.separator "Network options:"
+  opts.on('-f', '--fqdn HOSTNAME',  "Set the fully-qualified domain name to update (default: #{$domain_name})") {|n| $domain_name = n }
+  opts.on('-4', '--ipv4 INTERFACE', "Update A records for the IPv4 address on an interface") {|iface| $ipv4_interfaces << iface }
+  opts.on('-6', '--ipv6 INTERFACE', "Update AAAA records for global IPv6 addresses on an interface") {|iface| $ipv6_interfaces << iface }
+  opts.separator ""
+  opts.separator "DNS record options:"
+  opts.on('-t', '--ttl SECONDS',    "Set the time-to-live of the updated records (default: #{$ttl})") {|t| $ttl = t.to_i }
+  opts.on('-c', '--[no-]create',    "Create DNS records if they do not exist (default: #{$create_records})") {|v| $create_records = v }
+  opts.on('-w', '--[no-]wait',      "Wait for records to propagate before exiting (default: #{$await_propagation})") {|v| $await_propagation = v }
+  opts.separator ""
+  opts.separator "General options:"
+  opts.on('-v', '--verbose',        "Display status output about the script operations") { $verbose = true }
+  opts.on('-h', '--help',           "Display usage information") { print opts; exit }
 end
 options.parse!
 
@@ -43,12 +55,22 @@ def fail(msg, code=1)
   exit code
 end
 
+def log(msg)
+  puts msg if $verbose
+end
+
+def log_header(msg)
+  log "\n#{msg}"
+end
+
 fail options if ARGV.empty?
 $hosted_zone = ARGV.shift
 
 fail "Must specify at least one interface to check using --ipv4 or --ipv6" if $ipv4_interfaces.empty? && $ipv6_interfaces.empty?
 
 $domain_name << '.'
+
+log "Dynr53 run #{Time.now.strftime("%Y-%m-%dT%H:%M:%SZ")}"
 
 
 ##### IP ADDRESS DISCOVERY #####
@@ -78,16 +100,16 @@ end
 $ipv4_addresses = []
 $ipv6_addresses = []
 
-puts "Discovering global IP addresses..."
+log_header "Discovering global IP addresses..."
 System.get_all_ifaddrs.each do |info|
   interface = info[:interface]
   address = info[:inet_addr]
 
   if $ipv4_interfaces.include?(interface) && global_ipv4?(address)
-    puts "    #{interface}: #{address}"
+    log "    #{interface}: #{address}"
     $ipv4_addresses << address
   elsif $ipv6_interfaces.include?(interface) && global_ipv6?(address)
-    puts "    #{interface}: #{address}"
+    log "    #{interface}: #{address}"
     $ipv6_addresses << address
   end
 end
@@ -98,10 +120,10 @@ $ipv6_addresses.sort!
 
 ##### RECORD QUERYING #####
 
-puts "\nConnecting to AWS Route53 service..."
+log_header "Connecting to AWS Route53 service..."
 $route53 = Aws::Route53.new(region: 'us-west-2')
 
-puts "Querying Route53 for resource record sets for #{$domain_name}.."
+log "Querying Route53 for resource record sets for #{$domain_name}.."
 response = $route53.list_resource_record_sets(hosted_zone_id: $hosted_zone, start_record_name: $domain_name)
 
 record_sets = response.resource_record_sets.reject {|set| set.name != $domain_name }
@@ -111,10 +133,10 @@ $ipv6_record = nil
 record_sets.each do |set|
   if set.type == 'A'
     $ipv4_record = set.resource_records.map{|v| IPAddr.new(v.value) }.sort
-    puts "     A record: #{$ipv4_record.join(', ')} (ttl #{set.ttl})"
+    log "     A record: #{$ipv4_record.join(', ')} (ttl #{set.ttl})"
   elsif set.type == 'AAAA'
     $ipv6_record = set.resource_records.map{|v| IPAddr.new(v.value) }.sort
-    puts "     AAAA record: #{$ipv6_record.join(', ')} (ttl #{set.ttl})"
+    log "     AAAA record: #{$ipv6_record.join(', ')} (ttl #{set.ttl})"
   end
 end
 
@@ -131,9 +153,9 @@ def update_record(type, record, addresses)
 
     # Needs creating.
     if $create_records
-      puts "    #{type} record missing, creating => #{addresses.join(', ')}"
+      log "    #{type} record missing, creating => #{addresses.join(', ')}"
     else
-      puts "    #{type} record missing, not creating"
+      log "    #{type} record missing, not creating"
       return
     end
   else
@@ -141,7 +163,7 @@ def update_record(type, record, addresses)
     return if record == addresses
 
     # Needs updating.
-    puts "    #{type} record needs updating => #{addresses.join(', ')}"
+    log "    #{type} record needs updating => #{addresses.join(', ')}"
   end
 
   $updates << {
@@ -157,16 +179,16 @@ def update_record(type, record, addresses)
   }
 end
 
-puts "\nDetermining action..."
+log_header "Determining action..."
 update_record('A',    $ipv4_record, $ipv4_addresses)
 update_record('AAAA', $ipv6_record, $ipv6_addresses)
 
 if $updates.empty?
-  puts "    No updates required"
+  log "    No updates required"
   exit
 end
 
-puts "\nApplying updates to Route53..."
+log_header "Applying updates to Route53..."
 request = {
   hosted_zone_id: $hosted_zone,
   change_batch: {
@@ -179,19 +201,21 @@ response = $route53.change_resource_record_sets(request)
 
 change_id = response.change_info.id.split('/')[2]
 status = response.change_info.status
-puts "    Submitted change #{change_id} (#{response.change_info.status})"
+log "    Submitted change #{change_id} (#{response.change_info.status})"
 
 # Wait for any changes to succeed.
-max_retries = 6
-retry_period = 10
+if $await_propagation
+  max_retries = 6
+  retry_period = 10
 
-attempt = 0
-while status == 'PENDING' && attempt < max_retries
-  attempt += 1
-  sleep retry_period
-  response = $route53.get_change(id: change_id)
-  status = response.change_info.status
-  puts "    %d: %s%s" % [attempt, status, status == 'PENDING' ? '...' : '']
+  attempt = 0
+  while status == 'PENDING' && attempt < max_retries
+    attempt += 1
+    sleep retry_period
+    response = $route53.get_change(id: change_id)
+    status = response.change_info.status
+    log "    %d: %s%s" % [attempt, status, status == 'PENDING' ? '...' : '']
+  end
+
+  fail "Gave up after #{attempt * retry_period} seconds" if status != 'INSYNC'
 end
-
-fail "Gave up after #{attempt * retry_period} seconds" if status != 'INSYNC'
